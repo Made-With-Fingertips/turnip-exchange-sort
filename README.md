7cb23187d906e41e5b2c4ace80c2f692ddf8cedd
Ben Butzow <ben@butzow.me>
2020-07-24 23:24:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# turnip.exchange sort
+
+[![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/I3I01PVEO)
+
+This is a chrome extension that allows you to sort listings on [turnip.exchange](https://turnip.exchange/islands) by price, high to low.
+
+[![chrome](https://developer.chrome.com/webstore/images/ChromeWebStore_Badge_v2_496x150.png)](https://chrome.google.com/webstore/detail/turnip-exchange-sort/obmccfkghnlpgnkbmhnpooehpkgpmfkl?hl=en&authuser=0)
---
+++ content.js
@@ -0,0 +1,32 @@
+let rawNotes = document.getElementsByClassName("note");
+
+let notes = {};
+let prices = {};
+
+for (let index = 0; index < rawNotes.length; index++) {
+    const element = rawNotes[index];
+    let turnipCode = element.dataset.turnipCode;
+
+    if (turnipCode) {
+        notes[turnipCode] = element.outerHTML;
+
+        let price = parseInt(element.getElementsByTagName("p")[3].innerHTML.split(" ")[0]);
+        prices[turnipCode] = price;
+    }
+}
+
+let keys = Object.keys(prices);
+keys.sort(function(a, b) { return prices[b] - prices[a] });
+
+let noteContainer = document.getElementsByClassName("grid grid-flow-row grid-cols-2 sm:grid-cols-3 gap-8 justify-items-center items-center mb-4");
+if (noteContainer[0]) {
+    noteContainer[0].innerHTML = "";
+
+    let newHtml = "";
+    for (let index = 0; index < keys.length; index++) {
+        const element = keys[index];
+        newHtml += notes[element];
+    }
+
+    noteContainer[0].innerHTML = newHtml;
+}
