9cb7f16e7909e000d0fe117baa5c8dd4e1bfd3cf
Ben Butzow <ben@butzow.me>
2020-07-31 23:15:02 -0500

    Sort by waiting

--- popup.js
+++ popup.js
@@ -1,12 +1,26 @@
-let sortButton = document.getElementById("sort");
-sortButton.onclick = function(element) {
+let priceButton = document.getElementById("price");
+priceButton.onclick = function(element) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(tabs[0].id, {
-            file: "content.js"
+            file: "sortByPrice.js"
         }, function(result) {
-            sortButton.innerHTML = "SORTED!";
+            priceButton.innerHTML = "SORTED!";
             setTimeout(function() {
-                sortButton.innerHTML = "SORT BY PRICE";
+                priceButton.innerHTML = "SORT BY PRICE";
+            }, 5000);
+        });
+    });
+};
+
+let waitingButton = document.getElementById("waiting");
+waitingButton.onclick = function(element) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+            file: "sortByWaiting.js"
+        }, function(result) {
+            waitingButton.innerHTML = "SORTED!";
+            setTimeout(function() {
+                waitingButton.innerHTML = "SORT BY WAITING";
             }, 5000);
         });
     });
